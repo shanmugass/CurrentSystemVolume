8df9265615b42f37b30f7485359eab109ceaefa9
Shanmuga <shanmuga.sivagnanam@gmail.com>
2016-06-13 11:56:34 -0400

    Audio peek detection changes

--- CurrentSystemVolume/Program.cs
+++ CurrentSystemVolume/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using CSCore.CoreAudioAPI;
 
 namespace CurrentSystemVolume
 {
@@ -10,6 +13,73 @@ namespace CurrentSystemVolume
     {
         static void Main(string[] args)
         {
+            int noOfSecondsToWatch = 30;
+
+            var allPeekValues = new List<float>();
+
+            if (args.Length > 0)
+            {
+                if (!int.TryParse(args[0], out noOfSecondsToWatch))
+                {
+
+                }
+            }
+
+            try
+            {
+
+                using (var sessionManager = GetDefaultAudioSessionManager2(DataFlow.Render))
+                {
+                    using (var sessionEnumerator = sessionManager.GetSessionEnumerator())
+                    {
+                        for (int i = 0; i < noOfSecondsToWatch; i++)
+                        {
+                            foreach (var session in sessionEnumerator)
+                            {
+                                using (var audioMeterInformation = session.QueryInterface<AudioMeterInformation>())
+                                {
+                                    var audioPeek = audioMeterInformation.GetPeakValue();
+
+                                    Console.WriteLine(audioPeek);
+                                    allPeekValues.Add(audioPeek);
+                                }
+
+                                Thread.Sleep(1000);
+                            }
+                        }
+                    }
+                }
+
+
+                if (allPeekValues.Any())
+                {
+                    Console.WriteLine("Maximum Peek: " + allPeekValues.Max());
+                }
+                else
+                {
+                    Console.WriteLine("Maximum Peek: 0");
+                }
+
+            }
+            catch (Exception exception)
+            {
+                Console.Write("Unexpected error occured. Might be no audio device configured in your system." + exception.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static AudioSessionManager2 GetDefaultAudioSessionManager2(DataFlow dataFlow)
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(dataFlow, Role.Multimedia))
+                {
+                    Console.WriteLine("DefaultDevice: " + device.FriendlyName);
+                    var sessionManager = AudioSessionManager2.FromMMDevice(device);
+                    return sessionManager;
+                }
+            }
         }
     }
 }
